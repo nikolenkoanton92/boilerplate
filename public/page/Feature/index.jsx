76f5087cc015aa2740ff97e36065a2d9d5c56859
Anton Nikolenko <nikolenkoanton92@gmail.com>
2016-01-16 15:45:02 -0800

    add feature page content

--- public/page/Feature/index.jsx
+++ public/page/Feature/index.jsx
@@ -3,8 +3,20 @@ import React from 'react'
 class Feature extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Feature Page</h2>
+      <div className="container">
+        <div className="row">
+          <h2 className="text-center">Feature Page</h2>
+          <p className="text-center">Boilerplate includes such features </p>
+          <ul>
+            <li>JSPM</li>
+            <li>Express</li>
+            <li>React</li>
+            <li>Babel</li>
+            <li>React Router</li>
+            <li>Authentication</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
       </div>
       )
   }
